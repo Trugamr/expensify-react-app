eb49ff5d14d2194a7fdd79be11cf1db62d72f00b
Trugamr <6trugamr9@gmail.com>
2019-07-06 18:20:41 +0530

    added action generator tests

--- src/tests/actions/expenses.test.js
+++ src/tests/actions/expenses.test.js
@@ -1,9 +1,58 @@
 import { removeExpense, addExpense, editExpense } from '../../actions/expenses'
 
 test('should setup remove expense action object', () => {
-    const action= removeExpense({ id: 'a12dc4e' })
+    const action = removeExpense({ id: 'a12dc4e' })
     expect(action).toEqual({
         type: 'REMOVE_EXPENSE',
         id: 'a12dc4e'
+    })
+})
+
+test('should setup edit expense action object', () => {
+    const action = editExpense('aba2131s', {
+        description: 'Gas Bill',
+        note: 'July 2019 bill'
+    })
+    expect(action).toEqual({
+        id: 'aba2131s', 
+        type: 'EDIT_EXPENSE',
+        updates: {
+            description: 'Gas Bill',
+            note: 'July 2019 bill'
+        }
+    })
+})
+
+test('should setup add expense action object with provided values', () => {
+    const expenseData = {
+        description: 'Bill',
+        amount: 102000,
+        createdAt: 10000,
+        note: 'This is test expense'
+    }
+    const action = addExpense(expenseData)
+    expect(action).toEqual({
+        type: 'ADD_EXPENSE',
+        expense: {
+            id: expect.any(String),
+            ...expenseData
+        }
+    })
+})
+
+test('should setup add expense action object with default values', () => {
+    const defaultValues = {
+        description: '',
+        note: '',
+        amount: 0,
+        createdAt: 0 
+    }
+    const action = addExpense()
+    expect(action).toEqual({
+        type: 'ADD_EXPENSE',
+        expense: {
+            id: expect.any(String),
+            ...defaultValues
+        }
     })
 })
