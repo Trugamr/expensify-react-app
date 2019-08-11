c7f1c46991187b270b49e9e16a2a2d93192893cb
Trugamr <6trugamr9@gmail.com>
2019-08-11 21:24:36 +0530

    added dist folder

--- webpack.config.js
+++ webpack.config.js
@@ -6,7 +6,7 @@ module.exports = (env) => {
         // entry: './src/app.js',
         entry: './src/app.js',
         output: {
-            path: path.join(__dirname, 'public'),
+            path: path.join(__dirname, 'public', 'dist'),
             filename: 'bundle.js'
         },
         module: {
@@ -42,7 +42,8 @@ module.exports = (env) => {
         devServer: {
             contentBase: path.join(__dirname, 'public'),
             port: 9000,
-            historyApiFallback: true
+            historyApiFallback: true,
+            publicPath: '/dist/'
         }
     }
 }
