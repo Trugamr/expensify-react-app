4e0485094ebd47e6984b362c508c022c10aabb8d
Trugamr <6trugamr9@gmail.com>
2019-08-22 22:45:22 +0530

    added logout button and header tests

--- src/tests/components/Header.test.js
+++ src/tests/components/Header.test.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import { shallow } from 'enzyme'
-import Header from '../../components/Header'
+import { Header } from '../../components/Header'
 
 test('should render Header correctly', () => {
     const wrapper = shallow(<Header />)
@@ -10,4 +10,11 @@ test('should render Header correctly', () => {
     // renderer.render(<Header />)
     // expect(renderer.getRenderOutput()).toMatchSnapshot()
     // console.log(renderer.getRenderOutput())
+})
+
+test('should call startLogout on button click', () => {
+    const startLogout = jest.fn();
+    const wrapper = shallow(<Header startLogout={startLogout}/>);
+    wrapper.find('button').simulate('click');
+    expect(startLogout).toHaveBeenCalled();
 })
