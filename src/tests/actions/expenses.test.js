42cf1354e7216d451194869b3d98e27527caeb50
Trugamr <6trugamr9@gmail.com>
2019-08-29 01:39:43 +0530

    storing data in user area

--- src/tests/actions/expenses.test.js
+++ src/tests/actions/expenses.test.js
@@ -4,6 +4,8 @@ import configureMockStore from 'redux-mock-store'
 import database from '../../firebase/firebase'
 import expenses from '../fixtures/expenses'
 
+const uid ='testuid'
+const defaultAuthState = { auth: { uid }}
 const createMockStore = configureMockStore([thunk])
 
 beforeEach((done) => {
@@ -11,7 +13,7 @@ beforeEach((done) => {
     expenses.forEach(({ id, description, note, amount, createdAt }) => {
         expensesData[id] = { description, note, amount, createdAt }
     })
-    database.ref('expenses').set(expensesData).then(() => done())
+    database.ref(`users/${uid}/expenses`).set(expensesData).then(() => done())
 })
 
 test('should setup remove expense action object', () => {
@@ -23,7 +25,7 @@ test('should setup remove expense action object', () => {
 })
 
 test('should remove expense from firebase', (done) => {
-    const store = createMockStore({});
+    const store = createMockStore(defaultAuthState);
     const id = expenses[2].id;
     store.dispatch(startRemoveExpense({ id }))
         .then(() => {
@@ -32,7 +34,7 @@ test('should remove expense from firebase', (done) => {
                 type: 'REMOVE_EXPENSE',
                 id
             })
-            return database.ref(`expenses/${id}`).once('value')
+            return database.ref(`users/${uid}/expenses/${id}`).once('value')
         })
         .then((snap) => {
             expect(snap.val()).toBeFalsy();
@@ -61,7 +63,7 @@ test('should edit expense from firebase', (done) => {
         amount: 200
     }
     const id = expenses[0].id
-    const store = createMockStore({})
+    const store = createMockStore(defaultAuthState)
     store.dispatch(startEditExpense(id, updates))
         .then(() => {
             const actions = store.getActions()
@@ -70,7 +72,7 @@ test('should edit expense from firebase', (done) => {
                 id,
                 updates
             })
-            return database.ref(`expenses/${id}`).once('value')
+            return database.ref(`users/${uid}/expenses/${id}`).once('value')
         })
         .then(snap => {
             expect(snap.val()).toEqual({
@@ -99,7 +101,7 @@ test('should setup add expense action object with provided values', () => {
 })
 
 test('should add expense to database and store', (done) => {
-    const store = createMockStore()
+    const store = createMockStore(defaultAuthState)
     const expenseData = {
         description: 'Rent',
         amount: 3000,
@@ -116,7 +118,7 @@ test('should add expense to database and store', (done) => {
                     ...expenseData
                 }
             })
-            return database.ref(`expenses/${actions[0].expense.id}`)
+            return database.ref(`users/${uid}/expenses/${actions[0].expense.id}`)
                 .once('value')
                 
         })
@@ -127,7 +129,7 @@ test('should add expense to database and store', (done) => {
 })
 
 test('should add expense to database and store with default values', (done) => {
-    const store = createMockStore()
+    const store = createMockStore(defaultAuthState)
     const expenseData = {
         description: '',
         amount: 0,
@@ -144,7 +146,7 @@ test('should add expense to database and store with default values', (done) => {
                     ...expenseData
                 }
             })
-            return database.ref(`expenses/${actions[0].expense.id}`)
+            return database.ref(`users/${uid}/expenses/${actions[0].expense.id}`)
                 .once('value')
                 
         })
@@ -163,7 +165,7 @@ test('should setup set expense action object with data', () => {
 })
 
 test('should fetch the expenses from firebase', (done) => {
-    const store = createMockStore({})
+    const store = createMockStore(defaultAuthState)
     store.dispatch(startSetExpenses())
         .then(() => {
             const actions = store.getActions()
