a60222916ade1588e5d283443394e945a4a1e311
Trugamr <6trugamr9@gmail.com>
2019-09-02 00:31:54 +0530

    styled edit expenses page'

--- src/components/ExpenseForm.js
+++ src/components/ExpenseForm.js
@@ -42,7 +42,7 @@ class ExpenseForm extends React.Component {
     onSubmit = (e) => {
         e.preventDefault()
         if(!this.state.description || !this.state.amount) {
-            this.setState({ error: 'Please enter description and amount' })
+            this.setState({ error: 'Please enter description and amount.' })
         } else {
             this.setState({ error: '' })
             this.props.onSubmit({
@@ -55,44 +55,46 @@ class ExpenseForm extends React.Component {
     }
     render() {
         return (
-            <div>
-                <p>{this.state.error && this.state.error}</p>
-                <form onSubmit={ this.onSubmit }>
-                    <input
-                        className="input"
-                        type="text"
-                        placeholder="Description"
-                        autoFocus
-                        value={ this.state.description }
-                        onChange={ this.onDescriptionChange }
-                    />
-                    <input
-                        type="text"
-                        className="input"
-                        placeholder="Amount"
-                        value={ this.state.amount }
-                        onChange={ this.onAmountChange }
-                    />
-                    <SingleDatePicker
-                        date={ this.state.createdAt }
-                        onDateChange={ this.onDateChange }
-                        focused={ this.state.calendarFocused }
-                        onFocusChange={ this.onFocusChange }
-                        numberOfMonths={1}
-                        isOutsideRange={(day) => (false)}
-                        displayFormat={'MMM Do'}
-                        customInputIcon={<i className="fas fa-calendar-day"> </i>}
-                    />
-                    <textarea
-                        className="textarea"
-                        placeholder="Add a note for your expense (optional)"
-                        value={ this.state.note }
-                        onChange={ this.onNoteChange}
-                    >
-                    </textarea>
-                    <button>Add Expense</button>
-                </form>
-            </div>
+            <form className="form" onSubmit={ this.onSubmit }>
+                {this.state.error && <p className="form__error">
+                    {this.state.error}
+                </p>}
+                <input
+                    className="input"
+                    type="text"
+                    placeholder="Description"
+                    autoFocus
+                    value={ this.state.description }
+                    onChange={ this.onDescriptionChange }
+                />
+                <input
+                    type="text"
+                    className="input"
+                    placeholder="Amount"
+                    value={ this.state.amount }
+                    onChange={ this.onAmountChange }
+                />
+                <SingleDatePicker
+                    date={ this.state.createdAt }
+                    onDateChange={ this.onDateChange }
+                    focused={ this.state.calendarFocused }
+                    onFocusChange={ this.onFocusChange }
+                    numberOfMonths={1}
+                    isOutsideRange={(day) => (false)}
+                    displayFormat={'MMM Do, YYYY'}
+                    customInputIcon={<i className="fas fa-calendar-day"> </i>}
+                />
+                <textarea
+                    className="textarea"
+                    placeholder="Add a note for your expense (optional)"
+                    value={ this.state.note }
+                    onChange={ this.onNoteChange}
+                >
+                </textarea>
+                <button className="form__button button green">
+                    <i className="fas fa-save"></i> &nbsp;Save Expense
+                </button>
+            </form>
         )
     }
 }
