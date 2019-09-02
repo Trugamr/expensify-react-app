8976fc1fb04152524ed00b5d85e42eda6654280a
Trugamr <6trugamr9@gmail.com>
2019-09-02 16:26:44 +0530

    fixed expense form tests

--- src/tests/components/ExpenseForm.test.js
+++ src/tests/components/ExpenseForm.test.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import { shallow } from 'enzyme'
 import expenses from '../fixtures/expenses'
-import ExpenseForm from '../../components/ExpenseForm'
+import { ExpenseForm } from '../../components/ExpenseForm'
 import moment from 'moment'
 
 test('should render expense form correctly', () => {
