01bae0d1e546861e6ccb52c522991c215902c31e
Trugamr <6trugamr9@gmail.com>
2019-09-06 19:08:15 +0530

    added hidden expenses count when filtering

--- src/components/ExpensesSummary.js
+++ src/components/ExpensesSummary.js
@@ -7,14 +7,26 @@ import selectExpensesTotal from '../selectors/expenses-total'
 
 numeral.locale('in')
 
-export const ExpensesSummary = ({ expensesCount, expensesTotal }) => {
+export const ExpensesSummary = ({ expensesCount, expensesTotal, expensesHidden }) => {
     const fomattedExpensestotal = numeral(expensesTotal/100).format('$0,0.00') 
     const expenseWord = expensesCount == 1 ? 'expense' : 'expenses'
     return (
         <div className="page-header">
             <div className="content-container">
                 <div className="page_header__flex">
-                    <h1 className="page-header__title">Viewing <span>{expensesCount}</span> {expenseWord} totalling <span>{fomattedExpensestotal}</span></h1>
+                    <div className="page_header_left">
+                        <h1 className="page-header__title">
+                            Viewing <span>{expensesCount}</span> {expenseWord} totalling <span>{fomattedExpensestotal}</span>
+                        </h1>
+                        { !expensesHidden ? (
+                            <p className="page-header__sub-title">showing all expenses</p>
+                        ) : (
+                            <p className="page-header__sub-title">
+                                filtering out <span>{expensesHidden}</span> expenses
+                            </p>
+                        )}
+                    </div>
+                    
                     <div className="page-header__action"><Link to="/create" className="button yellow">
                         <i className="fas fa-plus"></i> &nbsp;Add Expense
                     </Link></div>
@@ -25,10 +37,12 @@ export const ExpensesSummary = ({ expensesCount, expensesTotal }) => {
 }
 
 const mapStateToProp = ({ expenses, filters }) => {
-    const viewableEpenses = selectExpenses(expenses, filters)
+    const viewableExpenses = selectExpenses(expenses, filters)
+    const hiddenExpenses = expenses.length - viewableExpenses.length;
     return {
-        expensesCount: viewableEpenses.length,
-        expensesTotal: selectExpensesTotal(viewableEpenses)
+        expensesCount: viewableExpenses.length,
+        expensesHidden:hiddenExpenses,
+        expensesTotal: selectExpensesTotal(viewableExpenses)
     }   
 }
 
